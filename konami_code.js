6a3be9dce38b2e21ffd593210063ee438f8b46cb
Jerry Brown <brownjer3@gmail.com>
2018-03-12 00:51:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
 var index = 0;
 
-document.body.addEventListener('keydown', function(e)) {
+document.addEventListener('keydown', function(e)) {
     var key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
@@ -15,9 +15,10 @@ document.body.addEventListener('keydown', function(e)) {
       index = 0;
       }
     }
-    else {
-      index = 0;
-    }
+
+  }
+  else {
+    index = 0;
   }
 }
 
