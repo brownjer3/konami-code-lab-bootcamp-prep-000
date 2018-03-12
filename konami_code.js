eebccae0637a761938c386722aa80a12cd7e6f30
Jerry Brown <brownjer3@gmail.com>
2018-03-12 01:45:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var index = 0;
+
 
 function init() {
-var body = document.body;
-
-  body.addEventListener('keydown', function(e) {
-      var key = parseInt(e.detail || e.which);
+var index = 0;
+  document.body.addEventListener('keydown', function(e) {
+      let key = parseInt(e.detail || e.which);
 
       if (key === code[index]) {
       index++;
 
-      if (index === code.length ) {
+        if (index === code.length-1) {
 
-      window.alert('YES.');
-      index = 0;
+          window.alert('YES.');
+          index = 0;
 }
   } else {
       index = 0;
